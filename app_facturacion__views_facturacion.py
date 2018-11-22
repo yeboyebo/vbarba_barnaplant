60b078096a8f12d868ef932451ff947710007a08
Nikolay-YeboYebo <nikolay.yeboyebo@gmail.com>
2018-11-22 16:35:07 +0100

    Añadir clientBch.py

---
+++ app_facturacion__views_facturacion.py
@@ -0,0 +1,9 @@
+
+# @class_declaration vbarba_barnaplant #
+class vbarba_barnaplant(vbarba_cabrera):
+    FILE_UPLOAD_DIR = '/mnt/carros/BARNAPLANT'
+    # FILE_UPLOAD_DIR = '/var/www/images'
+
+    def __init__(self, context=None):
+        super(vbarba_barnaplant, self).__init__(context)
+
